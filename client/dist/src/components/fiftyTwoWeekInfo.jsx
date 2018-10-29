097dee8ab55e5fd7d662dd394df00695c78004ed
Jonathan McNamara <jpmcnmr@gmail.com>
2018-10-29 14:58:36 -0700

    rendering lowest and highest below bottom line

---
+++ client/dist/src/components/fiftyTwoWeekInfo.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class FiftyTwoWeekInfo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      lowest: 0,
+      highest: 100,
+    }
+  }
+
+  render() {
+    return (
+      <div className='fiftyTwoWeek'>
+        <div className="lowest">
+          <span>52 week low </span>
+          <span>{this.state.lowest}</span>
+        </div>
+        <div className="highest">
+        <span>52 week high</span>
+        <span>{this.state.highest}</span>
+
+        </div>
+      </div>
+      //52 week low
+      //52 week high
+    )
+  }
+}
+
+export default FiftyTwoWeekInfo;
