ae301ec74f3d1563aeb58277644f87f6ad9be6af
Jonathan McNamara <jpmcnmr@gmail.com>
2018-10-30 10:35:31 -0700

    updated server

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
+
 const express = require('express');
+
 const path = require('path');
+
 const query = require('./modals/query.js');
+
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
-app.get('/company', (req ,res)=> {
-  query.get(1, (err, docs) => {
+app.get('/company/:id', (req ,res)=> {
+  const id = JSON.parse(req.params.id);
+  query.get(id, (err, docs) => {
     if (err) {
       res.status(500).send();
     } else {
       res.status(200).send(JSON.stringify(docs));
     }
-  } )
+  })
 
 });
 
